38f8d1eb4c1b0badcbe7e4c6d5d64ad403932a8d
Блоцкий Влад <Ultra_Mann@live.ru>
2022-03-14 23:03:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_17._6._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
